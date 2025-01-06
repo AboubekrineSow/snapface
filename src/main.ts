06f6ee93159ed8e99095125db8d4c6e163e174c7
Aboubakr <sow.aboubakrine1008@gmail.com>
2025-01-07 00:35:39 +0100

    add pipes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import * as fr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
 
+registerLocaleData(fr.default);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
